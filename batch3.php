e34bfcb248c14f2db83ed61432640e776a77d2b9
Richard Mahn <rich.mahn@unfoldingword.org>
2020-04-09 16:51:52 -0400

    Fixes

--- batch3.php
+++ batch3.php
@@ -7,14 +7,14 @@ require_once('assets/getid3/getid3.php');
 require_once('assets/getid3/write.php');
 
 $my_dir = dirname(__FILE__);
-if (! file_exists($my_dir."/podcasts_old.json")) {
-	die("NOT FOUND: ".$my_dir."/podcasts_old.json");
+if (!file_exists($my_dir . "/podcasts_old.json")) {
+	die("NOT FOUND: " . $my_dir . "/podcasts_old.json");
 }
-$podcasts_data = json_decode(file_get_contents($my_dir."/podcasts_old.json"), true);
+$podcasts_data = json_decode(file_get_contents($my_dir . "/podcasts_old.json"), true);
 $images_done = array();
-foreach($podcasts_data as $podcast){
+foreach ($podcasts_data as $podcast) {
 	// if($podcast['title'] != 'Encounters with Jesus | PART 3') continue;
-	print("=====================\n\nPROCESSING: ".$podcast['title']."\n");
+	print("=====================\n\nPROCESSING: " . $podcast['title'] . "\n");
 
 	$title = $podcast['title'];
 	$series = $podcast['series'];
@@ -40,7 +40,7 @@ foreach($podcasts_data as $podcast){
 	$audio_file_path = wp_upload_dir()['basedir']  . "/{$date['year_month']}/$audio_base";
 	$audio_link = wp_upload_dir()['baseurl'] . "/{$date['year_month']}/$audio_base";
 
-	if (! file_exists($audio_file_path)) {
+	if (!file_exists($audio_file_path)) {
 		print('MP3 does not exist: ' . $audio_file_path . "\n");
 		// UNCOMMENT IN PROD:
 		// die;
@@ -49,7 +49,7 @@ foreach($podcasts_data as $podcast){
 		$dir = dirname($audio_file_path);
 		if (!file_exists($dir)) {
 			mkdir($dir, 0777, true);
-		}    
+		}
 		copy("/home/jimgro7/old-rss/$audio_base", $audio_file_path);
 	}
 
@@ -58,7 +58,7 @@ foreach($podcasts_data as $podcast){
 	$image_file_path = null;
 	$image_link = null;
 	if (isset($podcast['img']) && $podcast['img']) {
-		$image_base = $upload_dir.explode('/jeremiah55683/', $podcast['img'])[1];
+		$image_base = $upload_dir . explode('/jeremiah55683/', $podcast['img'])[1];
 	} else {
 		$image_base = $date['file_date'] . ".jpg";
 	}
@@ -68,19 +68,19 @@ foreach($podcasts_data as $podcast){
 	} else {
 		$image_file_path = wp_upload_dir()['basedir'] . "/{$date['year_month']}/$image_base";
 		$image_link = wp_upload_dir()['baseurl']  . "/{$date['year_month']}/$image_base";
-		$images_done[$image_base] = array('path'=>$image_file_path, 'link'=>$image_link);
+		$images_done[$image_base] = array('path' => $image_file_path, 'link' => $image_link);
 	}
-	if (! file_exists($image_file_path)) {
-			print('IMAGE does not exist: '.$image_file_path."\n");
-			$dir = dirname($image_file_path);
-			if (!file_exists($dir)) {
-				mkdir($dir, 0777, true);
-			}
-			if (file_exists('/home/jimgro7/old-rss/'.$image_base)) {
-				copy('/home/jimgro7/old-rss/'.$image_base, $image_file_path);
-			} else {
-				copy('cover.jpg', $image_file_path);
-			}
+	if (!file_exists($image_file_path)) {
+		print('IMAGE does not exist: ' . $image_file_path . "\n");
+		$dir = dirname($image_file_path);
+		if (!file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
+		if (file_exists('/home/jimgro7/old-rss/' . $image_base)) {
+			copy('/home/jimgro7/old-rss/' . $image_base, $image_file_path);
+		} else {
+			copy('cover.jpg', $image_file_path);
+		}
 	}
 
 	$tagwriter = new getid3_writetags;
@@ -122,19 +122,19 @@ foreach($podcasts_data as $podcast){
 		die("Failed to write tags! Errors:\n" . implode("\n", $tagwriter->errors));
 	}
 
-	$audio_info = get_ID3( $audio_file_path );
+	$audio_info = get_ID3($audio_file_path);
 	unset($audio_info['image']);
 	print_r($audio_info);
 
 	// check if we have a title
-	if ( $title ) {
+	if ($title) {
 
-		$post_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type = 'wpfc_sermon' AND post_status = 'publish'", $title ) );
+		$post_id = $wpdb->get_var($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type = 'wpfc_sermon' AND post_status = 'publish'", $title));
 
 		// If there are no posts with the title of the sermon then make the sermon
-		if ( ! $post_id ) {
+		if (!$post_id) {
 
-			$tax_input_array = array (
+			$tax_input_array = array(
 				'wpfc_preacher'      => $speaker,
 				'wpfc_sermon_series' => $series,
 				'wpfc_service_type'  => 'Sunday Service',
@@ -151,56 +151,57 @@ foreach($podcasts_data as $podcast){
 				'post_type'   => 'wpfc_sermon',
 				'tax_input'   => $tax_input_array,
 				'post_content' => $desc,
-            );
-            
-            print_r(array(
-                'audio_base'=>$audio_base,
-                'audio_file_path'=>$audio_file_path,
-                'audio_link'=>$audio_link,
-                'image_base'=>$image_base,
-                'image_file_path'=>$image_file_path,
-                'image_link'=>$image_link,
-                'sermon_notes_base'=>$sermon_notes_base,
-                'sermon_notes_file_path'=>$sermon_notes_file_path,
-                'sermon_notes_link'=>$sermon_notes_link,
-                'my_post'=>$my_post,
-            ));
-			$post_id = wp_insert_post( $my_post );
+			);
 
-			$sql_prep = $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE guid = %s AND post_type = 'attachment' AND post_status = 'inherit'", $audio_link );
-			$audio_attach_id = $wpdb->get_var($sql_prep);
-			print_r(array('sql'=>$sql_prep, 'audio_attach_id'=>$audio_attach_id));
-			if ( ! $audio_attach_id ) {
-				print( 'No previous AUDIO attachment found.'."\n");
-				// UNCOMMENT in prod:
-				// die;
+			print_r(array(
+				'audio_base' => $audio_base,
+				'audio_file_path' => $audio_file_path,
+				'audio_link' => $audio_link,
+				'image_base' => $image_base,
+				'image_file_path' => $image_file_path,
+				'image_link' => $image_link,
+				'sermon_notes_base' => $sermon_notes_base,
+				'sermon_notes_file_path' => $sermon_notes_file_path,
+				'sermon_notes_link' => $sermon_notes_link,
+				'my_post' => $my_post,
+			));
+			$post_id = wp_insert_post($my_post);
+		}
 
-				// REMOVE/COMMENT-OUT in prod
-				$wp_filetype = wp_check_filetype( basename( $audio_file_path ), null );
-				$audio_attachment = array(
-					'post_mime_type' => $wp_filetype['type'],
-					'post_title'     => $title,
-					'post_content'   => $title.' by '.$speaker.' from '.$series.'. Released: '.$date['year'],
-					'post_date'      => $date['file_date'],
-					'post_status'    => 'inherit',
-					'guid'           => $audio_link,
-					'post_parent'    => $post_id,
-				);
-				$audio_attach_id = wp_insert_attachment( $audio_attachment, $audio_file_path, $post_id );
-                $attachment_data = wp_generate_attachment_metadata($audio_attach_id, $audio_file_path);
-                wp_update_attachment_metadata($audio_attach_id, $attachment_data);
-			}
+		$sql_prep = $wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid = %s AND post_type = 'attachment' AND post_status = 'inherit'", $audio_link);
+		$audio_attach_id = $wpdb->get_var($sql_prep);
+		print_r(array('sql' => $sql_prep, 'audio_attach_id' => $audio_attach_id));
+		if (!$audio_attach_id) {
+			print('No previous AUDIO attachment found.' . "\n");
+			// UNCOMMENT in prod:
+			// die;
+
+			// REMOVE/COMMENT-OUT in prod
+			$wp_filetype = wp_check_filetype(basename($audio_file_path), null);
+			$audio_attachment = array(
+				'post_mime_type' => $wp_filetype['type'],
+				'post_title'     => $title,
+				'post_content'   => $title . ' by ' . $speaker . ' from ' . $series . '. Released: ' . $date['year'],
+				'post_date'      => $date['file_date'],
+				'post_status'    => 'inherit',
+				'guid'           => $audio_link,
+				'post_parent'    => $post_id,
+			);
+			$audio_attach_id = wp_insert_attachment($audio_attachment, $audio_file_path, $post_id);
+			$attachment_data = wp_generate_attachment_metadata($audio_attach_id, $audio_file_path);
+			wp_update_attachment_metadata($audio_attach_id, $attachment_data);
+
 			$audio_attach_id = intval($audio_attach_id);
-			$rows_affected = $wpdb->update( 
-				$wpdb->posts, 
+			$rows_affected = $wpdb->update(
+				$wpdb->posts,
 				array(
-					'post_title' => $title, 
+					'post_title' => $title,
 					'post_date'  => $date['file_date'],
-					'post_content'=>$title.' by '.$speaker.' from '.$series.'. Released: '.$date['year'],
-					'post_excerpt'=>$comment,
+					'post_content' => $title . ' by ' . $speaker . ' from ' . $series . '. Released: ' . $date['year'],
+					'post_excerpt' => $comment,
 				),
-				array('ID' => $audio_attach_id), 
-				array('%s', '%s'), 
+				array('ID' => $audio_attach_id),
+				array('%s', '%s'),
 				array('%d')
 			);
 			$audio_attachment = get_post($audio_attach_id);
@@ -217,82 +218,82 @@ foreach($podcasts_data as $podcast){
 			wp_update_post($audio_art);
 
 			var_dump(array(
-				'update'=>array(
-					'post_title' => $title, 
-					'post_content'=>$title.' by '.$speaker.' from '.$series.'. Released: '.$date['year'],
-					'post_excerpt'=>$comment,
+				'update' => array(
+					'post_title' => $title,
+					'post_content' => $title . ' by ' . $speaker . ' from ' . $series . '. Released: ' . $date['year'],
+					'post_excerpt' => $comment,
 				),
-				'rows_affected'=>$rows_affected, 
-				'audio_attachment_data'=>$audio_attachment_data, 
-				'audio_attach_id' => $audio_attach_id, 
-				'audio_attachment'=>$audio_attachment
-				));
-			add_post_meta( $post_id, 'sermon_date', $date['unix_date'], $unique = false );
-			add_post_meta( $post_id, 'bible_passage', $audio['composer'], $unique = false );
-			add_post_meta( $post_id, 'sermon_audio', $audio_link, $unique = false );
-			add_post_meta( $post_id, '_wpfc_sermon_duration', $audio['length'], $unique = false );
-			add_post_meta( $post_id, '_wpfc_sermon_size', $audio['size'], $unique = false );
+				'rows_affected' => $rows_affected,
+				'audio_attachment_data' => $audio_attachment_data,
+				'audio_attach_id' => $audio_attach_id,
+				'audio_attachment' => $audio_attachment
+			));
 
-            if ($video)
-                add_post_meta( $post_id, 'sermon_video_link', $video, $unique = false );
-            if ($sermon_notes_link)
-                add_post_meta( $post_id, 'sermon_notes', $sermon_notes_link, $unique = false );
-            if ($desc)
-    			add_post_meta( $post_id, 'sermon_description', $desc, $unique = false );
+			add_post_meta($post_id, 'sermon_date', $date['unix_date'], $unique = false);
+			add_post_meta($post_id, 'bible_passage', $audio['composer'], $unique = false);
+			add_post_meta($post_id, 'sermon_audio', $audio_link, $unique = false);
+			add_post_meta($post_id, '_wpfc_sermon_duration', $audio['length'], $unique = false);
+			add_post_meta($post_id, '_wpfc_sermon_size', $audio['size'], $unique = false);
 
-			if ($image_link) {
-				$image_attach_id = null;
-				$image_attachment = null;
-
-				$sql_prep = $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE guid = %s AND post_type = 'attachment' AND post_status = 'inherit'", $image_link );
-				$image_attach_id = $wpdb->get_var( $sql_prep );
-				print_r(array('sql'=>$sql_prep, 'image_attach_id'=>$image_attach_id));
-
-				if ( ! $image_attach_id ) {
-					print( 'No previous image attachment found.'."\n");
-					// UNCOMMENT in prod:
-					// die;
-
-                    // REMOVE/COMMENT-OUT in prod
-					$wp_filetype = wp_check_filetype( basename( $image_file_path ), null );
-	 				$image_attachment = array(
-						'post_mime_type' => $wp_filetype['type'],
-					    'post_title'     => basename($image_file_path),
-						'post_date'      => $date['file_date'],
-						'post_status'    => 'inherit',
-					    'guid'           => $image_link,
-					    'post_parent'    => $post_id,
-					);
-				    $image_attach_id = wp_insert_attachment( $image_attachment, $image_file_path, $post_id );
-                    $attachment_data = wp_generate_attachment_metadata($image_attach_id, $image_file_path);
-                    wp_update_attachment_metadata($image_attach_id, $attachment_data);
-                }
-				$image_attachment = get_post($image_attach_id);
-				print_r(array($image_attach_id, $image_attachment));
-
-				$image_attachment->post_date = gmdate('Y-m-d H:i:s', strtotime($date['file_date']));
-				$image_attachment->post_date_gmt = gmdate('Y-m-d H:i:s', strtotime($date['file_date']));
-				wp_update_post($image_attachment);
-
-				$success = set_post_thumbnail($post_id, $image_attach_id);
-				if (!$success) {
-					echo "IMAGE THUMB FAILED\n";
-				} else {
-					echo "IMAGE THUMB SUCCESS\n";
-				}
-			}
-			$link = get_permalink( $post_id );
-			print('Sermon created: ' . $title . ' (' . $link . ")\n" );
-		} else {
-			print( 'Sermon already exists: ' . $title . "\n");
+			if ($video)
+				add_post_meta($post_id, 'sermon_video_link', $video, $unique = false);
+			if ($sermon_notes_link)
+				add_post_meta($post_id, 'sermon_notes', $sermon_notes_link, $unique = false);
+			if ($desc)
+				add_post_meta($post_id, 'sermon_description', $desc, $unique = false);
 		}
+
+		if ($image_link) {
+			$image_attach_id = null;
+			$image_attachment = null;
+
+			$sql_prep = $wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid = %s AND post_type = 'attachment' AND post_status = 'inherit'", $image_link);
+			$image_attach_id = $wpdb->get_var($sql_prep);
+			print_r(array('sql' => $sql_prep, 'image_attach_id' => $image_attach_id));
+
+			if (!$image_attach_id) {
+				print('No previous image attachment found.' . "\n");
+				// UNCOMMENT in prod:
+				// die;
+
+				// REMOVE/COMMENT-OUT in prod
+				$wp_filetype = wp_check_filetype(basename($image_file_path), null);
+				$image_attachment = array(
+					'post_mime_type' => $wp_filetype['type'],
+					'post_title'     => basename($image_file_path),
+					'post_date'      => $date['file_date'],
+					'post_status'    => 'inherit',
+					'guid'           => $image_link,
+					'post_parent'    => $post_id,
+				);
+				$image_attach_id = wp_insert_attachment($image_attachment, $image_file_path, $post_id);
+				$attachment_data = wp_generate_attachment_metadata($image_attach_id, $image_file_path);
+				wp_update_attachment_metadata($image_attach_id, $attachment_data);
+			}
+			$image_attachment = get_post($image_attach_id);
+			print_r(array($image_attach_id, $image_attachment));
+
+			$image_attachment->post_date = gmdate('Y-m-d H:i:s', strtotime($date['file_date']));
+			$image_attachment->post_date_gmt = gmdate('Y-m-d H:i:s', strtotime($date['file_date']));
+			wp_update_post($image_attachment);
+
+			$success = set_post_thumbnail($post_id, $image_attach_id);
+			if (!$success) {
+				echo "IMAGE THUMB FAILED\n";
+			} else {
+				echo "IMAGE THUMB SUCCESS\n";
+			}
+		}
+		$link = get_permalink($post_id);
+		print('Sermon created: ' . $title . ' (' . $link . ")\n");
 	}
 }
 
-function get_ID3( $file_path ) {
+function get_ID3($file_path)
+{
 	// Initialize getID3 engine
 	$get_ID3 = new getID3;
-	$ThisFileInfo = $get_ID3->analyze( $file_path );
+	$ThisFileInfo = $get_ID3->analyze($file_path);
 
 	$imageWidth = "";
 	$imageHeight = "";
@@ -301,50 +302,48 @@ function get_ID3( $file_path ) {
 	 * metadata is all available in one location for all tag formats
 	 * meta information is always available under [tags] even if this is not called
 	 */
-	getid3_lib::CopyTagsToComments( $ThisFileInfo );
+	getid3_lib::CopyTagsToComments($ThisFileInfo);
 
-	$tags = array( 'title' => sanitize_text_field( $ThisFileInfo['filename'] ), 'genre' => '', 'artist' => '', 'album' => '', 'year' => '' );
+	$tags = array('title' => sanitize_text_field($ThisFileInfo['filename']), 'genre' => '', 'artist' => '', 'album' => '', 'year' => '');
 
-	if ( !isset( $ThisFileInfo['tags']['id3v2'] ) ) {
-		die( $ThisFileInfo['filename'] . ' does not seem to have ID3 version 2 tags.'."\n");
+	if (!isset($ThisFileInfo['tags']['id3v2'])) {
+		die($ThisFileInfo['filename'] . ' does not seem to have ID3 version 2 tags.' . "\n");
 	}
 
-	foreach ( $tags as $key => $tag ) {
-		if ( isset( $ThisFileInfo['tags']['id3v2'] ) && array_key_exists( $key, $ThisFileInfo['tags']['id3v2'] ) ) {
-			$value = sanitize_text_field( $ThisFileInfo['tags']['id3v2'][$key][0] );
+	foreach ($tags as $key => $tag) {
+		if (isset($ThisFileInfo['tags']['id3v2']) && array_key_exists($key, $ThisFileInfo['tags']['id3v2'])) {
+			$value = sanitize_text_field($ThisFileInfo['tags']['id3v2'][$key][0]);
 			$tags[$key] = $value;
 		}
 	}
 
-	if ( isset( $ThisFileInfo['comments_html']['comment'] ) ) {
-		$value = sanitize_text_field( $ThisFileInfo['comments_html']['comment'][0] );
+	if (isset($ThisFileInfo['comments_html']['comment'])) {
+		$value = sanitize_text_field($ThisFileInfo['comments_html']['comment'][0]);
 		$tags['comment'] = $value;
 	}
 
 	// see en.wikipedia.org/wiki/ID3
 	// subtitle tit3 in id3v2 and tsst in id3v4
-	if ( ! empty( $ThisFileInfo['id3v2']['TIT3'][0]['data'] ) ) {
-		$tags['subtitle'] = sanitize_text_field( $ThisFileInfo['id3v2']['TIT3'][0]['data'] );
-	}
-	else if ( ! empty( $ThisFileInfo['id3v2']['TSST'][0]['data'] ) ) {
-			$tags['subtitle'] = sanitize_text_field( $ThisFileInfo['id3v2']['TSST'][0]['data'] );
-		}
-	else {
+	if (!empty($ThisFileInfo['id3v2']['TIT3'][0]['data'])) {
+		$tags['subtitle'] = sanitize_text_field($ThisFileInfo['id3v2']['TIT3'][0]['data']);
+	} else if (!empty($ThisFileInfo['id3v2']['TSST'][0]['data'])) {
+		$tags['subtitle'] = sanitize_text_field($ThisFileInfo['id3v2']['TSST'][0]['data']);
+	} else {
 		$tags['subtitle'] = '';
 	}
 
-	$tags['composer'] = empty( $ThisFileInfo['id3v2']['TCOM'][0]['data'] ) ? '' : sanitize_text_field( $ThisFileInfo['id3v2']['TCOM'][0]['data'] );
-	$tags['bitrate'] = sanitize_text_field( $ThisFileInfo['bitrate'] );
-	$tags['length'] = sanitize_text_field( $ThisFileInfo['playtime_string'] );
-	$tags['size'] = sanitize_text_field( $ThisFileInfo['filesize'] );
+	$tags['composer'] = empty($ThisFileInfo['id3v2']['TCOM'][0]['data']) ? '' : sanitize_text_field($ThisFileInfo['id3v2']['TCOM'][0]['data']);
+	$tags['bitrate'] = sanitize_text_field($ThisFileInfo['bitrate']);
+	$tags['length'] = sanitize_text_field($ThisFileInfo['playtime_string']);
+	$tags['size'] = sanitize_text_field($ThisFileInfo['filesize']);
 
-	if ( isset( $ThisFileInfo['comments']['picture'][0] ) ) {
+	if (isset($ThisFileInfo['comments']['picture'][0])) {
 		$pictureData = $ThisFileInfo['comments']['picture'][0];
 		$imageinfo = array();
 		//$imagechunkcheck = getid3_lib::GetDataImageSize($pictureData['data'], $imageinfo);
 		$imageWidth = "150"; //$imagechunkcheck[0];
 		$imageHeight = "150"; //$imagechunkcheck[1];
-		$tags['image'] = '<img src="data:'.$pictureData['image_mime'].';base64,'.base64_encode( $pictureData['data'] ).'" width="'.$imageWidth.'" height="'.$imageHeight.'" class="img-polaroid">';
+		$tags['image'] = '<img src="data:' . $pictureData['image_mime'] . ';base64,' . base64_encode($pictureData['data']) . '" width="' . $imageWidth . '" height="' . $imageHeight . '" class="img-polaroid">';
 	}
 
 	return $tags;
