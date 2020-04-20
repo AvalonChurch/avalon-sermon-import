511fbae3552278214f7d70adc706d680932fc705
Richard Mahn <rich.mahn@unfoldingword.org>
2020-04-19 23:39:19 -0400

    Fixes size and adds links

--- update_sermons.php
+++ update_sermons.php
@@ -291,7 +291,7 @@ function update_sermons()
 		$ret[] = update_post_meta($post->ID, 'sermon_description', $meta_sermon_description, $meta['sermon_description'][0]);
 		$ret[] = update_post_meta($post->ID, 'bible_passage', $bible_passage, $meta['bible_passage'][0]);
 		$ret[] = update_post_meta($post->ID, 'sermon_audio', $meta_sermon_audio, $meta['sermon_audio'][0]);
-		$ret[] = update_post_meta($post->ID, '_wpfc_sermon_size', $audio_filesize, (isset($meta['_wpfc_sermon_size'])?$meta['_wpfc_sermon_size']:null))
+		$ret[] = update_post_meta($post->ID, '_wpfc_sermon_size', $audio_filesize, (isset($meta['_wpfc_sermon_size'])?$meta['_wpfc_sermon_size']:null));
 
 		print_r($bible_book_items);
 		$ret[] = wp_set_post_terms($post->ID, $bible_book_items, 'wpfc_bible_book', true);
